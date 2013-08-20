1a7cdf95a240746d91ef6f7f4ad9680236756093
aemcasts <aemcasts@mba.local>
2013-08-20 17:40:31 -0400

    Initial commit of episode 1

---
+++ bundle/src/main/java/com/adobe/aemcasts/demo/HelloService.java
@@ -0,0 +1,13 @@
+package com.adobe.aemcasts.demo;
+
+/**
+ * A simple service interface
+ */
+public interface HelloService {
+    
+    /**
+     * @return the name of the underlying JCR repository implementation
+     */
+    public String getRepositoryName();
+
+}
---
+++ bundle/src/main/java/com/adobe/aemcasts/demo/package-info.java
@@ -0,0 +1,4 @@
+@Version("1.0.0")
+package com.adobe.aemcasts.demo;
+
+import aQute.bnd.annotation.Version;
